cec014711d4e2d09d38f2fecac4931c7882e6747
Evhen <sonykillstation@gmail.com>
2022-05-21 18:55:39 +0300

    Capcha

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -2,12 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from captcha.fields import CaptchaField
 
 from .models import *
+
+
 class AddPostForm(forms.ModelForm):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = "Категория не выбрана"
+
     class Meta:
         model = Women
         fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
@@ -16,12 +20,15 @@ class AddPostForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10})
 
         }
+
     def clean_title(self):
         title = self.cleaned_data['title']
-        if len(title)>200:
+        if len(title) > 200:
             raise ValidationError('Длинна превышает 200 символов')
         return title
         # fields ='__all__'
+
+
 # class AddPostForm(forms.Form):
 #
 #     title = forms.CharField(max_length=255, label="Заголовок",widget=forms.TextInput(attrs={'class': 'form-input'}))
@@ -40,7 +47,14 @@ class RegisterUserForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label="Email")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+    captcha = CaptchaField()
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import *
 from .models import *
@@ -50,9 +50,22 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         c_def = self.get_user_context(title="Добавление статьи")
         return dict(list(context.items()) + list(c_def.items()))
 
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
 
-def contact(request):
-    return HttpResponse("Обратная связь")
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items()) + list(c_def.items()))
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
+
+#def contact(request):
+#    return HttpResponse("Обратная связь")
 
 
 # def login(request):
